0563fab10022b156a12ba0e49535bc12851ea525
俞南松 <505772146@qq.com>
2014-12-17 17:09:40 +0800

    quickcocos2dx的折线运动和曲线运动

--- scripts/app/scenes/MainScene.lua
+++ scripts/app/scenes/MainScene.lua
@@ -1,3 +1,4 @@
+--quici-cocos2d-x  2.2.5
 local MainScene = class("MainScene", function()
 	return display.newScene("MainScene")
 end)
@@ -6,17 +7,43 @@ function MainScene:ctor()
 	local a = display.newSprite("a.png", 100, 100)
 	self:addChild(a)
 
-	array = CCPointArray:create(3)
-	array:add(ccp(100, 100))
-	array:add(ccp(350,150))
-	array:add(ccp(500, 170))
+	--这里曲线运动是self:quxianMove()，而折线运动是self:zhexianMove()，到时候相应地注释就可以看效果了。
+	a:runAction(self:quxianMove())
+	--a:runAction(self:zhexianMove())
+end
 
-	local action = CCCardinalSplineTo:create(5, array, 0)
-	a:runAction(action)
+function MainScene:zhexianMove()
+	local array = CCPointArray:create(5)
+	array:add(ccp(100, 100))
+	array:add(ccp(150, 150))
+	array:add(ccp(200, 100))
+	array:add(ccp(150, 50))
+	array:add(ccp(100, 100))
+
+	--CCCatmullRomTo 折线运动
+	--参数1：运动时间
+	--参数2：运动的轨迹点坐标集合
+	local action = CCCatmullRomTo:create(1, array)
+	return action
+end
+
+function MainScene:quxianMove()
+	local array = CCPointArray:create(4)
+	array:add(ccp(100, 100))
+	array:add(ccp(200, 200))
+	array:add(ccp(300, 250))
+	array:add(ccp(400, 275))
+
+	--CCCardinalSplineTo 曲线运动
+	--参数1：运动时间
+	--参数2：运动的轨迹点坐标集合
+	--参数3:0表示曲线最柔和
+	local action = CCCardinalSplineTo:create(1, array, 0)
+	return action
 end
 
 function MainScene:onEnter()
-	
+
 end
 
 return MainScene
